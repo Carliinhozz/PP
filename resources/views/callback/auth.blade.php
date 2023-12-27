8d9093a50a1602035d7ca756d490f80d87c2a012
Cecilia Sousa <cecilia.sousa@escolar.ifrn.edu.br>
2023-12-27 11:08:44 -0300

    voltando o auth.blade

--- resources/views/callback/auth.blade.php
+++ resources/views/callback/auth.blade.php
@@ -11,60 +11,59 @@
 
 <body>
 
-    <div id="overlay">
-        <div id="loading-text">Carregando...</div>
-    </div>
+    carregando
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-        integrity="sha384-oBqDVmMz9AT
-        KxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-        crossorigin="anonymous"></script>
-    
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
-        crossorigin="anonymous"></script>
+        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="js/js.cookie.js"></script>
     <script src="js/client.js"></script>
     
     <script>
-       $("#overlay").show();
-            var suap = new SuapClient(
-                "{{ config('suap.uri') }}",
-                "{{ config('suap.client_id') }}",
-                "{{ config('suap.redirect_uri') }}",
-                "{{ config('suap.scope') }}"
-            );
-            suap.init();
-            if (suap.isAuthenticated()) {
-                const suapToken = suap.getToken().getValue();
-                const csrfToken = document.getElementsByName('_token')[0].value;
-                $.ajax({
-                    url: '/authorization-callback',
-                    data: {
-                        '_token': csrfToken,
-                        'suap_token': suapToken,
-                    },
-                    type: 'POST',
-                    success: function (response) {
-                        window.location = '/'
-                    },
-                    error: function (response) {
-                        alert('Falha na comunicação com o servidor');
-                        console.log(response);
-                    },
-                    complete: function () {
-                        $("#overlay").hide();
-                    }
-                });
-            } else {
-                alert('A autenticação via SUAP falhou.');
-                window.location = "/";
+      var suap = new SuapClient(
+        "{{ config('suap.uri') }}",
+        "{{ config('suap.client_id') }}",
+        "{{ config('suap.redirect_uri') }}", 
+        "{{ config('suap.scope') }}"
+      );
+      suap.init();
+      if (suap.isAuthenticated()) {
+        // Aguarda o documento carregar para exibir o conteúdo
+        $(document).ready(function () {
+          // Token de autenticação do SUAP
+          const suapToken = suap.getToken().getValue();
+          // Token CSRF fornecido pelo Laravel
+          const csrfToken = document.getElementsByName('_token')[0].value;
+          
+          // Envia a o token do SUAP para o callback
+          $.ajax({
+            url: '/authorization-callback',
+            data: {
+              '_token' : csrfToken,
+              'suap_token' : suapToken,
+            },
+            type: 'POST',
+            success: function(response) {
+              window.location='/'
+            },
+            error: function(response) {
+              alert('Falha na comunicação com o servidor');
+              console.log(response);
             }
+          });
         });
+      } else {
+        // O usuário não está autenticado
+        alert('A autenticação via SUAP falhou.');
+        window.location = "/";
+      }
     </script>
-    @csrf
+    @csrf <!-- Necessário para evitar ataques CSRF, não remova -->
   </body>
 </html>
